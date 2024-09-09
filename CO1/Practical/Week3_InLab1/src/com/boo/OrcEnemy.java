9b1b5d7e710070c91173080e407a33b95606e875
phadkeshreya <shreyaphadke2005@gmail.com>
2024-09-09 20:28:30 +0530

    Add files via upload
    JAVA code for  a game where there are multiple levels with different difficulty settings

---
+++ CO1/Practical/Week3_InLab1/src/com/boo/OrcEnemy.java
@@ -0,0 +1,7 @@
+package com.boo;
+
+public class OrcEnemy implements Enemy {
+	public void attack() {
+        System.out.println("Orc enemy attack!");
+    }
+}
---
+++ CO1/Practical/Week3_InLab1/src/com/boo/ShieldPowerUp.java
@@ -0,0 +1,7 @@
+package com.boo;
+
+public class ShieldPowerUp implements PowerUp {
+	 public void activate() {
+	        System.out.println("Shield power-up activated!");
+	    }
+}
