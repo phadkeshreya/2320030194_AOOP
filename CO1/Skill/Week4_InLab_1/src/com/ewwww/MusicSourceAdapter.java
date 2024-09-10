c877b118a929bfd30a7f20da83c80e397f2833c2
phadkeshreya <shreyaphadke2005@gmail.com>
2024-09-10 21:10:43 +0530

    Add files via upload
    Skill 2
    JAVA code to develop Music Streaming Application

---
+++ CO1/Skill/Week4_InLab_1/src/com/ewwww/MusicSourceAdapter.java
@@ -0,0 +1,13 @@
+package com.ewwww;
+
+public class MusicSourceAdapter implements MusicSource {
+	private MusicSource musicSource;
+
+    public MusicSourceAdapter(MusicSource musicSource) {
+        this.musicSource = musicSource;
+    }
+
+    public void play() {
+        musicSource.play();
+    }
+}
