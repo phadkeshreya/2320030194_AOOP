1d6d9295070990825284ba2bf101fd1320794c97
phadkeshreya <shreyaphadke2005@gmail.com>
2024-09-10 21:40:01 +0530

    Delete CO1/Practical/Week4_InLab1 directory

--- CO1/Practical/Week4_InLab1/src/com/image/AdvancedImageViewer.java
+++
@@ -1,6 +0,0 @@
-package com.image;
-
-public interface AdvancedImageViewer {
-	void showPng(String fileName);
-    void showJpg(String fileName);
-}
--- CO1/Practical/Week4_InLab1/src/com/image/ImageViewer.java
+++
@@ -1,5 +0,0 @@
-package com.image;
-
-public interface ImageViewer {
-	void show(String imageType, String fileName);
-}
