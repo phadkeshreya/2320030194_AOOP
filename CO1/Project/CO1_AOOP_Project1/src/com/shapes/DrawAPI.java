416dbef9d8d54b0899474990c0a1ed61389c67e3
phadkeshreya <shreyaphadke2005@gmail.com>
2024-09-13 13:29:18 +0530

    CO1 Project Shapes

---
+++ CO1/Project/CO1_AOOP_Project1/src/com/shapes/DrawAPI.java
@@ -0,0 +1,5 @@
+package com.shapes;
+
+public interface DrawAPI {
+	public String drawCircle(int radius, int x, int y);
+}
---
+++ CO1/Project/CO1_AOOP_Project1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CO1_AOOP_Project1 {
+	requires org.junit.jupiter.api;
+}
